5fe50f1ae5384e5a15fd752869215976c6775d50
Evandro-Cant <evandro.souza@smarkio.com.br>
2021-08-16 10:52:28 -0300

    Temperature layout

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import Card from "./componentes/Card";
 import { useState, useEffect } from "react";
 
 function App() {
+  const kelvinToCelsius = (kelvin) => kelvin - 273.15;
   const [weather, setWeather] = useState([]);
   async function getWeather() {
     let res = await fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=Niteroi&appid=1da18ab91e28724e1792e906e9d3cda8"
     );
     let resJson = await res.json();
-    setWeather(resJson)
+    setWeather(resJson);
   }
+  var data = new Date();
+  console.log(data.toLocaleDateString());
   console.log(weather);
+  console.log(Math.floor(kelvinToCelsius(292.97)));
   useEffect(() => {
     getWeather();
   }, []);
   return (
-    <div className='container'>
-      <div className='box'>
-        <Card cityName={weather.name}></Card>
-      </div>
+    <div className="container">
+      {(typeof weather.main != "undefined") ? (
+              <div className="box">
+              <Card
+                cityName={weather.name}
+                data={data.toLocaleDateString()}
+                tempe={Math.floor(kelvinToCelsius(weather.main.temp))}
+              ></Card>
+            </div>
+      ) : ('')}
     </div>
   );
 }
--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 
 // import { Container } from './styles';
 
 function Card(props) {
-
   return (
-      <div className='corpo'>
-          <div className='tela'>
-              <div className='cont'>
-                  <h3>Clima</h3>
-                  <h4>{props.cityName}</h4>
-              </div>
+    <div className="corpo">
+      <div className="tela">
+        <div className="cont">
+          <div className="top">
+            <h3>{props.cityName}</h3>
+            <p>{props.data}</p>
           </div>
+          <p>Temperatura: {props.tempe}º </p>
+        </div>
       </div>
+    </div>
   );
 }
 
